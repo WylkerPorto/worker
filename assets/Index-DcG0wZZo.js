e52730ecd6cfc2fd98ed19d11160ca8f22161a68
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-04-10 18:52:17 +0000

    deploy: 97f145c57065418b9250f816f6d222cf3f900afd

--- assets/Index-DcG0wZZo.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as t}from"./index-YQsSOIm2.js";const c={name:"RecruterIndex"};function n(o,a,s,d,_,p){return t(),r("div",null,"recruter dashboard")}const f=e(c,[["render",n]]);export{f as default};
