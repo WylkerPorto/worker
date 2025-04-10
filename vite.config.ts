2d8a18af04236432018d953899f8ac984935dadb
Wylker Porto <dragonester@hotmail.com>
2025-04-10 11:34:03 -0300

    chore: atualiza versão do pacote para 0.0.1 e adiciona configuração de deploy para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://github.com/WylkerPorto/worker',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
