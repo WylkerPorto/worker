a7487dac07b3b5aca7aa2399285bea88d7a91204
Wylker Porto <dragonester@hotmail.com>
2025-04-10 16:11:41 -0300

    fix: remove commented line and adjust formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/worker/',
@@ -14,5 +12,14 @@ export default defineConfig({
   },
   server: {
     port: 8080,
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
   },
 })
