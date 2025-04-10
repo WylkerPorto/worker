ccbd3158eab9e91691b92808376534d8d05188ce
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-04-10 19:12:09 +0000

    deploy: a7487dac07b3b5aca7aa2399285bea88d7a91204

---
+++ assets/Index-aR6kT3Xl.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as r}from"./index-CfFh_1Yf.js";const s={name:"SupervisorIndex"};function n(t,a,c,p,d,_){return r(),o("div",null,"supervisor dashboard")}const u=e(s,[["render",n]]);export{u as default};
