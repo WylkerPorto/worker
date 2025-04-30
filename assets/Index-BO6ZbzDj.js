ef8428b669ec9f1036750bd09a24deb2afa955ac
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-04-30 00:47:49 +0000

    deploy: 5e2c6f7c0b33bcb129a5c474847116682fe09d82

---
+++ assets/Index-BO6ZbzDj.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t}from"./index-2jy5Q05I.js";const c={name:"RecruterIndex"};function n(o,a,s,d,_,p){return t(),r("div",null,"recruter dashboard")}const f=e(c,[["render",n]]);export{f as default};
--- assets/user-Dt7GlIK-.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./http-j0rTDgai.js";import{t as r}from"./ModalBase-D0JD0HBS.js";const u=t=>s.authApi().post("/user",t),p=t=>s.authApi().get(`/user?${r(t)}`),i=(t,e)=>s.authApi().put(`/user/${t}`,e),c=t=>s.authApi().delete(`/user/${t}`);export{u as c,p as l,c as r,i as u};
