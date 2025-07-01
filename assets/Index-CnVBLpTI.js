a13f0d3a627c31459b2ea4419d08f6cf3d0e4627
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-07-01 01:59:52 +0000

    deploy: 8bf979bdbbae7f115f76c68737618020c9b871ea

---
+++ assets/Index-CnVBLpTI.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t}from"./index-KmYJkn1P.js";const c={name:"RecruterIndex"};function n(o,a,s,d,_,p){return t(),r("div",null,"recruter dashboard")}const f=e(c,[["render",n]]);export{f as default};
