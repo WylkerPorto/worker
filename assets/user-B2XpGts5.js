1ea694894f6435ffe877af4a53c2a564740d9e2d
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-07-15 00:27:54 +0000

    deploy: 436c4d86d6597da6c83ef0385b13568bcc78e578

--- assets/user-B2XpGts5.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./http-zF2HLkK1.js";import"./index-S2YWwiI0.js";const a=t=>e.authApi().post("/person",t),r=t=>e.authApi().get(`/person/${t}`),n=(t,p)=>e.authApi().put(`/person/${t}`,p),u=t=>e.authApi().delete(`/person/${t}`);export{a as c,r as g,u as r,n as u};
