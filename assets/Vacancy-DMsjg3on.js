fabc0a130e867f89fbb0f990ced782837f044da1
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-07-22 00:58:09 +0000

    deploy: 17730f0e800c634d07d18a38194d1578a48960a3

---
+++ assets/Vacancy-DMsjg3on.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-BEa9HEip.js";const a={name:"RecruiterVacancy"};function n(o,r,s,_,p,f){return t(),c("div",null,"teste")}const l=e(a,[["render",n]]);export{l as default};
