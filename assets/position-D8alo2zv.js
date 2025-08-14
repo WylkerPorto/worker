b29f932f391fbe7cebf9cc7c349e87558da93d59
WylkerPorto <WylkerPorto@users.noreply.github.com>
2025-08-14 00:58:09 +0000

    deploy: b665bf985b1704247ba395a44d00003a5ff2fa9b

---
+++ assets/position-D8alo2zv.js
@@ -0,0 +1,1 @@
+import{h as o}from"./http-CmPME1wD.js";import{t as s}from"./conversors-DMBNotbN.js";const e=t=>o.authApi().post("/position",t),r=t=>o.authApi().get(`/position?${s(t)}`),n=(t,i)=>o.authApi().put(`/position/${t}`,i),u=t=>o.authApi().delete(`/position/${t}`);export{e as c,r as l,u as r,n as u};
