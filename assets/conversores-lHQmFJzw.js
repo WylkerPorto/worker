22a07f47e886a21536a4940363de1a6c20f5baf7
WylkerPorto <WylkerPorto@users.noreply.github.com>
2026-07-06 20:21:51 +0000

    deploy: 66b17996addca8ea95a7404585c9b1068d0cdab1

---
+++ assets/conversores-lHQmFJzw.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-BIU24ZLB.js";const s=t=>{if(!t)return"";const o=Object.fromEntries(Object.entries(t).filter(([n,r])=>r!=null&&r!==""));return new URLSearchParams(o).toString()},c=t=>t?e.utc(t).format("DD/MM/YYYY"):"";export{s as a,c as t};
---
+++ assets/department-BFyPI4Wp.js
@@ -0,0 +1,1 @@
+import{h as e}from"./http-cFAw7v0k.js";import{a as p}from"./conversores-lHQmFJzw.js";const s=t=>e.authApi().post("/department",t),m=t=>e.authApi().get(`/department?${p(t)}`),n=(t,a)=>e.authApi().put(`/department/${t}`,a),i=t=>e.authApi().delete(`/department/${t}`);export{s as c,m as l,i as r,n as u};
