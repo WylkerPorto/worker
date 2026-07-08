be4d186a8e6a7f59f63ec83f55e04ff987efcbbb
WylkerPorto <WylkerPorto@users.noreply.github.com>
2026-07-08 20:45:55 +0000

    deploy: 48c173c309689093836844906ed719fd3f1c7e36

---
+++ assets/position-BwgArk1L.js
@@ -0,0 +1,1 @@
+import{h as o}from"./http-DxAQ0Hbc.js";import"./index-B74iKhj4.js";const a=t=>o.authApi().post("/position",t),e=()=>o.authApi().get("/position"),n=(t,i)=>o.authApi().put(`/position/${t}`,i),r=t=>o.authApi().delete(`/position/${t}`);export{a as c,e as l,r,n as u};
