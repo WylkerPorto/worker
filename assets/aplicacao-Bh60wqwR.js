8634c060f84fdb241fe3620dc95353e2b7a28530
WylkerPorto <WylkerPorto@users.noreply.github.com>
2026-07-14 18:28:06 +0000

    deploy: 4c3643225584c0ad856ff230d8c6a9cfd937648b

---
+++ assets/aplicacao-Bh60wqwR.js
@@ -0,0 +1,1 @@
+import{h as p}from"./http-KukUzAT6.js";import{a as c}from"./conversores-Dj7ZRUrJ.js";const s=a=>p.authApi().post("/application",a),e=(a,t)=>p.authApi().put(`/application/${a}`,t),n=(a,t)=>p.authApi().get(`/application/vacancy/${a}?${c(t)}`),r=(a,t)=>p.authApi().get(`/application/person/${a}?${c(t)}`);export{r as a,s as c,n as g,e as u};
