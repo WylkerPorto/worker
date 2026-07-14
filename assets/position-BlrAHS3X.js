f3e0fcb0aee1ffee3125d89b9a0bbc9ea434a1a7
WylkerPorto <WylkerPorto@users.noreply.github.com>
2026-07-14 19:13:41 +0000

    deploy: 38f2158e669ee9664b139c5528a7162539737ef9

---
+++ assets/position-BlrAHS3X.js
@@ -0,0 +1,1 @@
+import{h as o}from"./http-DA4TGBOK.js";import"./index-DvtpPW-L.js";const a=t=>o.authApi().post("/position",t),e=()=>o.authApi().get("/position"),n=(t,i)=>o.authApi().put(`/position/${t}`,i),r=t=>o.authApi().delete(`/position/${t}`);export{a as c,e as l,r,n as u};
